8525a5d3fad0e7c8b1f3d4a59b6625b9b3763e88
Victor Martinez <vicmtzdev@gmail.com>
2023-08-28 16:54:54 -0600

    first-commit

---
+++ src/auth/pages/LoginPage.jsx
@@ -0,0 +1,78 @@
+import './LoginPage.css';
+
+export const LoginPage = () => {
+    return (
+        <div className="container login-container">
+            <div className="row justify-content-center">
+                <div className="col-md-6 login-form-1">
+                    <h3>Login</h3>
+                    <form>
+                        <div className="form-group mb-2">
+                            <input 
+                                type="text"
+                                className="form-control"
+                                placeholder="E-mail"
+                            />
+                        </div>
+                        <div className="form-group mb-2">
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Password"
+                            />
+                        </div>
+                        <div className="d-grid gap-2">
+                            <input 
+                                type="submit"
+                                className="btnSubmit"
+                                value="Login" 
+                            />
+                        </div>
+                    </form>
+                </div>
+
+                <div className="col-md-6 login-form-2">
+                    <h3>New account</h3>
+                    <form>
+                        <div className="form-group mb-2">
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Name"
+                            />
+                        </div>
+                        <div className="form-group mb-2">
+                            <input
+                                type="email"
+                                className="form-control"
+                                placeholder="E-mail"
+                            />
+                        </div>
+                        <div className="form-group mb-2">
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Password" 
+                            />
+                        </div>
+
+                        <div className="form-group mb-2">
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Confirm password" 
+                            />
+                        </div>
+
+                        <div className="d-grid gap-2">
+                            <input 
+                                type="submit" 
+                                className="btnSubmit" 
+                                value="Create account" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/calendar/index.js
@@ -0,0 +1,5 @@
+
+export * from './pages/CalendarPage';
+
+
+export * from './components/Navbar';
